3aea800163a0292aad42668e9bcfa7e7d968337f
Rubeldevops <mohaiminul972@gmail.com>
2025-01-05 23:46:14 +0600

    Update run.py

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from app import create_app
 
 app = create_app()
 
-if _name_ == '_main_':
-    app.run(host='0.0.0.0', port=5000
+if __name__ == "__main__":  # Corrected this line
+    app.run(host='0.0.0.0', port=5000)
